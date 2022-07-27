18c9a287b969d17d1e8347c453805ae129e68892
김태용 <amico741@naver.com>
2022-07-28 00:27:14 +0900

    Feat: 인증 Guard 생성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { Token } from './entity/token.entity';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Token]), UsersModule],
-  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([Token]), UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
