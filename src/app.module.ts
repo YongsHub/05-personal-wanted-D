fc5ba94298a92c18d82be3a355574f9e458afbd7
김태용 <amico741@naver.com>
2022-07-28 15:42:08 +0900

    Setting: posts Module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 import * as ormConfig from '../ormConfig';
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), AuthModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(ormConfig), AuthModule, UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
