146bf6ba44687e25e2863cea5d8e94aaa6c70c94
김태용 <amico741@naver.com>
2022-07-29 16:56:12 +0900

    Refactor: 해시태그 중복 방지

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -18,7 +18,29 @@ export class PostsService {
   async createPost(createPostDto: PostCreateDto) {
     const { title, content, hashtags } = createPostDto;
     //console.log(`제목:${title}`, `내용:${content}`, `해시태그: ${hashtags}`);
-    const tag = await this.hashtagsRepository.findBy({ title: In(hashtags) });
-    console.log(tag);
+    const tags = await this.hashtagsRepository.findBy({ title: In(hashtags) });
+    const tagsList: HashTag[] = [];
+    let newHashTag: HashTag;
+    hashtags.forEach(async (hashtag) => {
+      let isExist = false;
+      tags.forEach((tag) => {
+        if (tag.title === hashtag) {
+          isExist = true;
+          newHashTag = tag;
+        }
+      });
+      if (!isExist) {
+        newHashTag = new HashTag(hashtag);
+        await this.hashtagsRepository.save(newHashTag);
+        tagsList.push(newHashTag);
+      } else {
+        tagsList.push(newHashTag);
+      }
+    });
+
+    const post = new Post(title, content);
+    post.hashtags = tagsList;
+
+    await this.postsRepository.save(post);
   }
 }
